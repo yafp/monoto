ecbacb95dd8a6df9844ba5625070cc3a4db0b3fb
fidel <fidel@macfidelity.de>
2012-07-07 20:20:15 +0200

    fixed update--check with files hosted on github

--- info.php
+++ info.php
@@ -500,51 +500,46 @@ if ( isset($_POST["doUpdateCheck"]) )
 	$critical = FALSE;
 	$update = FALSE;
 
-	if (!$_SESSION['updateCheck']) 
-	{
-		$url = "https://raw.github.com/macfidelity/monoto/master/versionCheck.csv";
-		$fp = @fopen ($url, 'r') or print ('UPDATE SERVER OFFLINE');
-		$read = fgetcsv ($fp);
-		fclose ($fp); //always a good idea to close the file connection
-		$_SESSION['updateCheck'] = TRUE;
+	$url = "https://raw.github.com/macfidelity/monoto/master/versionCheck.csv";
+	$fp = @fopen ($url, 'r') or print ('UPDATE SERVER OFFLINE');
+	$read = fgetcsv ($fp);
+	fclose ($fp); //always a good idea to close the file connection
 
-		// its critical
-		if (($read[0] > $m_build) && ($read[2] == "1")) 
-		{ 
-			$critical = TRUE; 
-		}
+	// its critical
+	if (($read[0] > $m_build) && ($read[2] == "1")) 
+	{ 
+		$critical = TRUE; 
+	}
 		
-		// normal update
-		if ($read[0] > $m_build) 
-		{ 
-			$update = TRUE; 
-		}
+	// normal update
+	if ($read[0] > $m_build) 
+	{ 
+		$update = TRUE; 
+	}
 
-
-		if ($critical) 
-		{ 
+	if ($critical) 
+	{ 
 			// print '<p>'.$read[1].'</p>';   													// aka buildbeschreibung
 			// print '<p>You can get it at <a href="'.$read[3].'">'.$read[3].'</a></p>';		// aka DL-url
 
-   			echo '<script type="text/javascript">
-
+   		echo '<script type="text/javascript">
    				var r=confirm("There is a critical update available. Should i download the latest version?")
 				if (r==true)
   				{
   					window.location = "https://raw.github.com/macfidelity/monoto/master/versionCheck.csv","_blank"; 
   				}
-   			</script>';
+   		</script>';
 
-			die(); //terminate the script
-		}
-		else if ($update)
-		{
-			echo '<script type="text/javascript">alert("There is an update available.");</script>';
-		}
-		else // uptodate
-		{
-			echo '<script type="text/javascript">alert("You are using the latest version. Well done.");</script>';
-		}
-	} // session 
+		die(); //terminate the script
+	}
+	else if ($update)
+	{
+		echo '<script type="text/javascript">alert("There is an update available.");</script>';
+	}
+	else // uptodate
+	{
+		echo '<script type="text/javascript">alert("You are using the latest version. Well done.");</script>';
+	}
+
 }
 ?>
