05c8cb50239b4df93b740146b372d6fc9c9f8aaa
fidel <fidel@macfidelity.de>
2012-08-06 16:32:39 +0200

    20120806.02 - yet another git test on netbook

--- conf/config.php
+++ conf/config.php
@@ -38,6 +38,6 @@
 	$m_name						= "monoto";						// name of your notes system
 	$m_milestone				= "3";							// current milestone number
 	$m_milestone_title			= "pimping the workflow";		// current milestone name
-	$m_build					= "20120729.01";				// build-date and day's version
+	$m_build					= "20120806.02";				// build-date and day's version
 	$m_stable					= false;						// defined if that is a milestone release
 ?>
--- doc/README.txt
+++ doc/README.txt
@@ -15,5 +15,3 @@ Keep in mind:
 This file is static - for current informations/full details consider visiting the project documentation at
 
 	https://github.com/macfidelity/monoto/wiki
-
-Ole ;)
