34e781fd9fdec19d443d13ed45b75351c3d33e06
macfidelity <fidel@macfidelity.de>
2014-03-26 20:15:27 +0100

    20140326.01 - Adding more german quotes and closing #187 (worked already)

--- conf/build.php
+++ conf/build.php
@@ -6,6 +6,6 @@
 	$m_name					= "monoto";							// name of your notes system
 	$m_milestone			= "3";								// current milestone number
 	$m_milestone_title	= "the pimp";		// current milestone name
-	$m_build					= "20140325.01";					// build-date and day's version
+	$m_build					= "20140326.01";					// build-date and day's version
 	$m_stable				= false;								// defined if that is a milestone release
 ?>
