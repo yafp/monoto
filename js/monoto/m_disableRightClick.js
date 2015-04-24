dca1f2fc87f3499963e44b441d13d63faa53a930
yafp <fidel@macfidelity.de>
2015-04-25 01:00:00 +0200

    20150425.01 -reorganizing the location of monoto self-written js scripts -> /js/monoto

---
+++ js/monoto/m_disableRightClick.js
@@ -0,0 +1,7 @@
+/* 
+DISABLING the right click - used for the entire interface
+*/
+$(document).bind("contextmenu",function(e)
+{
+	return false;
+});
---
+++ js/monoto/m_keyPressNotes.js
@@ -0,0 +1,81 @@
+/* 
+KEY PRESS SCRIPT - via: http://www.geekpedia.com/tutorial138_Get-key-press-event-using-JavaScript.html 
+KeyPress Codes: http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+*/
+
+// SINGLE KEY PRESSES
+//
+document.onkeyup = KeyCheck;       
+function KeyCheck(e)
+{
+	var KeyID = (window.event) ? event.keyCode : e.keyCode;
+	switch(KeyID)
+	{	
+		// F1 - Open online help/docs
+		case 112:
+			window.open('https://github.com/yafp/monoto/wiki');
+		break;
+
+
+
+		// F2 - trigger maximize editor to fullscreen
+		case 113:
+			CKEDITOR.instances.editor1.execCommand( 'maximize' );
+		break;
+
+
+
+		// ESC - reset the UI
+		case 27:
+			resetNotesUI();
+		break;
+
+
+
+		// Arrow Up
+		case 38:
+			// specialcase: if focus in search -> jump to first record in table
+			if(document.activeElement.id == "myInputTextField")
+			{
+				selectUpperRow();
+			}
+		break;
+
+
+
+		// Arrow Down
+		case 40:
+		   	// specialcase: if focus in search -> jump to first record in table
+			if(document.activeElement.id == "myInputTextField")
+			{
+				selectNextRow();
+			}
+		break;
+
+
+
+		// DEL - delete selected note & reloads page
+		case 46:
+			// missing: we should do that only if a row in datatables is selected 
+			// if focus is not in new title & in search & in noteTitle
+			if((document.activeElement.id != "newNoteTitle") && (document.activeElement.id != "noteTitle") && (document.activeElement.id != "myInputTextField"))	
+			{
+				deleteNote();								// execute delete function
+			}
+		break;
+
+
+
+		// F5 - Reload main page
+		case 116:
+			reloadNote();									// execute reload function
+		break;
+
+
+
+		// F9 - save
+		case 120:
+			saveNote();										// execute save function
+		break;
+	}
+}
