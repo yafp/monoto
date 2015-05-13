dda229d3690d27b701d642fff74160122d99a999
yafp <fidel@macfidelity.de>
2015-05-13 22:47:24 +0200

    20150513.01 - minor changes to footer - pw reset now with icon instead of text link

--- inc/footer.php
+++ inc/footer.php
@@ -3,7 +3,8 @@
 	<link rel="stylesheet" href="images/font-awesome-4.3.0/css/font-awesome.min.css">
 	<?php require 'conf/build.php'; ?>
 	<small>
-		<b><?php echo $m_name; ?></b> - build: <?php echo $m_build; ?> - Forgotten your password? <a href="pwreset.php">click here</a><br>
-		<a href="https://github.com/yafp/monoto" title="visit monoto at github"><i class="fa fa-github fa-2x"></i></a>
+		<a href="https://github.com/yafp/monoto" title="visit monoto at github"><i class="fa fa-github fa-1x"></i></a>
+		<b><?php echo $m_name; ?></b> - build: <?php echo $m_build; ?>
+		<a href="pwreset.php" title="password reset"><i class="fa fa-key fa-1x"></i></a>
 	</small>
 </div>
