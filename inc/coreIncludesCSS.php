e7c510b4abba54bee7631fb9fe8af4f9eb2fbb79
yafp <fidel@yafp.de>
2016-05-16 11:24:59 +0200

    20160516 - Moving more files to coreIncludes

--- inc/coreIncludesCSS.php
+++ inc/coreIncludesCSS.php
@@ -1,7 +1,7 @@
 <!-- CSS -->
 
 <!-- Font Awesome -->
-<link rel="stylesheet" type="text/css" href="images/font-awesome-4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="images/font-awesome/css/font-awesome.min.css">
 
 <!-- Bootstrap -->
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
--- inc/coreIncludesJS.php
+++ inc/coreIncludesJS.php
@@ -5,3 +5,15 @@
 
 <!-- Bootstrap -->
 <script src="js/bootstrap.min.js"></script>
+
+<!-- noty -->
+<script type="text/javascript" src="js/noty/jquery.noty.js"></script>
+<script type="text/javascript" src="js/noty/layouts/topRight.js"></script>
+<script type="text/javascript" src="js/noty/themes/default.js"></script>
+<script type="text/javascript" src="js/monoto/m_initNoty.js"></script>
+
+<!-- really logout -->
+<script type="text/javascript" src="js/monoto/m_reallyLogout.js"></script>
+
+<!-- disable rightclick -->
+<script type="text/javascript" src="js/monoto/m_disableRightClick.js"></script>	
