b2ec0455b549e2cd6b7afb100d5104b771589d3e
yafp <fidel@yafp.de>
2019-02-01 23:30:15 +0100

    20190201 - Renamed logout script

---
+++ js/monoto/m_logoutDialog.js
@@ -0,0 +1,28 @@
+// -----------------------------------------------------------------------------
+// Displays a logout dialog
+// -----------------------------------------------------------------------------
+function showLogoutDialog()
+{
+    functionName = "showLogoutDialog";
+    logToConsole(functionName, "");
+
+    var x = noty({
+        text: 'Do you really want to logout?',
+        //text: '<?php echo "foo"; ?>Do you really want to logout?',
+        type: "confirm",
+        dismissQueue: false,
+        layout: "topRight",
+        theme: "defaultTheme",
+        buttons: [
+            {addClass: "btn btn-primary", text: "Ok", onClick: function($noty) {
+                $noty.close();
+                window.location.href = "logout.php";
+            }
+        },
+        {addClass: "btn btn-danger", text: "Cancel", onClick: function($noty) {
+            $noty.close();
+        }
+    }
+]
+})
+}
