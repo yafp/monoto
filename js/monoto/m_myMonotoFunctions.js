f1f0f0c6edd392f08f88b2e21051aa1d9717478d
yafp <fidel@yafp.de>
2019-02-02 18:12:25 +0100

    20190202 - Fixing several minor issues

--- js/monoto/m_myMonotoFunctions.js
+++ js/monoto/m_myMonotoFunctions.js
@@ -3,29 +3,29 @@
 // -----------------------------------------------------------------------------
 function deleteAllMyUserEvents()
 {
-    functionName = "deleteAllMyUserEvents";
+    var functionName = "deleteAllMyUserEvents";
     logToConsole(functionName, "");
 
-	var x = noty({
-		text: 'Really delete all your events from log?',
-		type: 'confirm',
-		dismissQueue: false,
-		layout: 'topRight',
-		theme: 'defaultTheme',
-		buttons: [
-			{addClass: 'btn btn-primary', text: 'Ok', onClick: function($noty) {
-				$noty.close();
-				$.post("inc/delMyUserEvents.php");
-				$.cookie("lastAction", "Deleted all your event-entries."); // store last Action in cookie
-				noty({text: 'Deleted all events from log', type: 'success'});
-				location.reload();
-			}
-		},
-		{addClass: 'btn btn-danger', text: 'Cancel', onClick: function($noty) {
-			$noty.close();
-			noty({text: 'Aborted', type: 'error'});
-		}
-	}
+    var x = noty({
+        text: "Really delete all your events from log?",
+        type: "confirm",
+        dismissQueue: false,
+        layout: "topRight",
+        theme: "defaultTheme",
+        buttons: [
+            {addClass: 'btn btn-primary', text: 'Ok', onClick: function($noty) {
+                $noty.close();
+                $.post("inc/delMyUserEvents.php");
+                $.cookie("lastAction", "Deleted all your event-entries."); // store last Action in cookie
+                noty({text: "Deleted all events from log", type: "success"});
+                location.reload();
+            }
+        },
+        {addClass: 'btn btn-danger', text: 'Cancel', onClick: function($noty) {
+            $noty.close();
+            noty({text: "Aborted", type: "error"});
+        }
+    }
 ]
 });
 }
@@ -36,28 +36,28 @@ function deleteAllMyUserEvents()
 // -----------------------------------------------------------------------------
 function deleteAllMyUserNotes()
 {
-    functionName = "deleteAllMyUserNotes";
+    var functionName = "deleteAllMyUserNotes";
     logToConsole(functionName, "");
 
     var x = noty({
-    	text: 'Really delete all your notes?',
-    	type: 'confirm',
-    	dismissQueue: false,
-    	layout: 'topRight',
-    	theme: 'defaultTheme',
-    	buttons: [
-    		{addClass: 'btn btn-primary', text: 'Ok', onClick: function($noty) {
-    			$noty.close();
-    			$.post("inc/delMyUserNotes.php");
-    			$.cookie("lastAction", "Deleted all your notes."); // store last Action in cookie
-    			noty({text: 'Deleted all notes', type: 'success'});
-    			location.reload();
-    		}
-    	},
-    	{addClass: 'btn btn-danger', text: 'Cancel', onClick: function($noty) {
-    		$noty.close();
-    		noty({text: 'Aborted', type: 'error'});
-    	}
+        text: "Really delete all your notes?",
+        type: "confirm",
+        dismissQueue: false,
+        layout: "topRight",
+        theme: "defaultTheme",
+        buttons: [
+            {addClass: 'btn btn-primary', text: 'Ok', onClick: function($noty) {
+                $noty.close();
+                $.post("inc/delMyUserNotes.php");
+                $.cookie("lastAction", "Deleted all your notes."); // store last Action in cookie
+                noty({text: "Deleted all notes", type: "success"});
+                location.reload();
+            }
+        },
+        {addClass: 'btn btn-danger', text: 'Cancel', onClick: function($noty) {
+            $noty.close();
+            noty({text: "Aborted", type: "error"});
+        }
     }
     ]
 });
