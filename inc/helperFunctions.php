da78b54052442003214d73991fdf3982114776e7
fidel <yafp@users.noreply.github.com>
2019-02-07 13:48:06 +0100

    Update helperFunctions.php

--- inc/helperFunctions.php
+++ inc/helperFunctions.php
@@ -13,19 +13,32 @@ function displayNoty($notyText, $notyType)
 	// warning
 	// notification
 	// success
-	echo "<script>var n = noty({text: '".$notyText."', type: '".$notyType."'});</script>";	// display notification
+
+    // display notification
+	echo "<script>var n = noty({text: '".$notyText."', type: '".$notyType."'});</script>";
 }
 
 
+// -----------------------------------------------------------------------------
+// Update the cookie text (via javascript)
+// -----------------------------------------------------------------------------
+function updateCookie($message)
+{
+    writeToConsoleLog("updateCookie ::: Trying to update the cookie with value : _".$message."_.");
+
+    echo "<script>$.cookie('lastAction', '".$message."');</script>";
+
+    writeToConsoleLog("updateCookie ::: Finished updating the cookie.");
+}
+
 
 // -----------------------------------------------------------------------------
-// Write to JavaScript developer console
+// Write to JavaScript console (via javascript)
 // -----------------------------------------------------------------------------
 function writeToConsoleLog($message)
 {
-	echo "<script>console.log('".$message."')</script>"; // write to js console
+	echo "<script>console.log('[PHP]".$message."')</script>"; // write to js console
 }
-
 
 
 // -----------------------------------------------------------------------------
@@ -33,40 +46,51 @@ function writeToConsoleLog($message)
 // -----------------------------------------------------------------------------
 function translateString($textForTranslation)
 {
-	if ($_SESSION['getText'] == 0)
-	//if (!function_exists("gettext")) // gettext is not installed - fallback
+	if ($_SESSION['getText'] == 0) // gettext is not installed - fallback
 	{
 		$translation = $textForTranslation;
+        return $translation;
 	}
-	else // gettext is installed - translate
+
+	// otherwise: gettext is installed -> try to translate
+
+	// I18N support information here
+	$language = $_SESSION['lang'];
+	if($language == "") // Fallback to english
 	{
-		// I18N support information here
-		$language = $_SESSION['lang'];
-		if($language == "") // Fallback to english
+		$language = "en_US";
+	}
+
+	putenv("LANG=$language");
+	setlocale(LC_ALL, $language);
+
+	// Set the text domain as 'messages'
+	$domain = 'monoto';
+	bindtextdomain($domain, "./locale");
+	textdomain($domain);
+
+	$translation = gettext($textForTranslation);
+
+	// report non-translated texts for debugging
+	if($translation == $textForTranslation)
+	{
+		if($language != "en_US")
 		{
-			$language = "en_US";
-		}
-
-		putenv("LANG=$language");
-		setlocale(LC_ALL, $language);
-
-		// Set the text domain as 'messages'
-		$domain = 'monoto';
-		bindtextdomain($domain, "./locale");
-		textdomain($domain);
-
-		$translation = gettext($textForTranslation);
-
-		// report non-translated texts for debugging
-		if($translation == $textForTranslation)
-		{
-			if($language != "en_US")
-			{
-				writeToConsoleLog("Translation-issue: _".$textForTranslation."_");
-			}
+			writeToConsoleLog("translateString ::: Translation-issue, found no translation for: _".$textForTranslation."_.");
 		}
 	}
+
 	return $translation;
 }
+
+
+// -----------------------------------------------------------------------------
+// Playground for phpunit
+// -----------------------------------------------------------------------------
+function odd_or_even( $num )
+{
+    return $num%2; // Returns 0 for odd and 1 for even
+}
+
 
 ?>
