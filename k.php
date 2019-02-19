a58fb0917a85b091657d1500811bf61ef0847ac5
fpoeck <f.poeck@kniel.de>
2019-02-19 15:20:36 +0100

    20190219 - Improving keyboard page - Fixing #283

--- k.php
+++ k.php
@@ -4,6 +4,9 @@
 <html lang="en">
 <head>
     <?php include 'inc/coreIncludes.php'; ?>
+    <!-- specific -->
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/monoto/keyboard.css" title="default" />
 </head>
 
 <body role="document">
@@ -17,15 +20,15 @@
             <h3><?php echo translateString("notes-page"); ?></h3>
             <table class="tableWithBorders" style="width:100%">
                 <tr><th width="25%"><?php echo translateString("Key"); ?></th><th><?php echo translateString("Function"); ?></th></tr>
-                <tr><td>ESC</td><td><?php echo translateString("Resets all input fields and sets focus to search"); ?></td></tr>
-                <tr><td>F2</td><td><?php echo translateString("Toggle maximize of editor"); ?></td></tr>
-                <tr><td>F5</td><td><?php echo translateString("Reloads all notes from db"); ?></td></tr>
-                <tr><td>F9</td><td><?php echo translateString("Saves a selected note"); ?></td></tr>
-                <tr><td><?php echo translateString("Del"); ?></td><td><?php echo translateString("Deletes the selected note"); ?></td></tr>
-                <tr><td><i class="fas fa-arrow-down"></i> <?php echo translateString("(in search)"); ?></td><td><?php echo translateString("Selects the top record"); ?></td></tr>
-                <tr><td><i class="fas fa-arrow-down"></i> <?php echo translateString("(with selected note)"); ?></td><td><?php echo translateString("Selects the next record"); ?></td></tr>
-                <tr><td><i class="fas fa-arrow-up"></i> <?php echo translateString("(in search)"); ?></td><td><?php echo translateString("Selects the last record"); ?></td></tr>
-                <tr><td><i class="fas fa-arrow-up"></i> <?php echo translateString("(with selected note)"); ?></td><td><?php echo translateString("Selects the previous record"); ?></td></tr>
+                <tr><td><div class="key special"><span>ESC</span></div></td><td><?php echo translateString("Resets all input fields and sets focus to search"); ?></td></tr>
+                <tr><td><div class="key special"><span>F2</span></div></td><td><?php echo translateString("Toggle maximize of editor"); ?></td></tr>
+                <tr><td><div class="key special"><span>F5</span></div></td><td><?php echo translateString("Reloads all notes from db"); ?></td></tr>
+                <tr><td><div class="key special"><span>F9</span></div></td><td><?php echo translateString("Saves a selected note"); ?></td></tr>
+                <tr><td><div class="key special"><span><?php echo translateString("Del"); ?></span></div></td><td><?php echo translateString("Deletes the selected note"); ?></td></tr>
+                <tr><td><div class="key special"><span><i class="fas fa-arrow-down"></i></span></div> <?php echo translateString("(in search)"); ?></td><td><?php echo translateString("Selects the top record"); ?></td></tr>
+                <tr><td><div class="key special"><span><i class="fas fa-arrow-down"></i></span></div> <?php echo translateString("(with selected note)"); ?></td><td><?php echo translateString("Selects the next record"); ?></td></tr>
+                <tr><td><div class="key special"><span><i class="fas fa-arrow-up"></i></span></div> <?php echo translateString("(in search)"); ?></td><td><?php echo translateString("Selects the last record"); ?></td></tr>
+                <tr><td><div class="key special"><span><i class="fas fa-arrow-up"></i></span></div> <?php echo translateString("(with selected note)"); ?></td><td><?php echo translateString("Selects the previous record"); ?></td></tr>
             </table>
 
             <!-- footer -->
